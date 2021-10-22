4e61b445ffa7988d76571e0fc0ff685a4ee74582
René Link <rene.link@link-intersystems.com>
2021-10-22 09:52:41 +0200

    Removed test index.ts

--- src/__tests__/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "../proxy/__tests__/consoleProxy.test";
-export * from "../interceptors/__tests__/consoleInterceptors.test";
-export * from "../template/__tests__/consoleTemplate.test";
